fe84dfee6f6eef48d9775fde49968361af09f08f
100poisha <100poisha@gmail.com>
2014-03-26 05:10:27 +0900

    add Option class

---
+++ Source/Maybe/Maybe.Test/Option.cs
@@ -0,0 +1,264 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Maybe.Test
+{
+	[TestFixture]
+    public class OptionTest
+    {
+        //Option<T>
+        [Test]
+        public void Default_struct()
+        {
+            var o = default(Option<int>);
+            o.HasValue.IsFalse();
+            Assert.Catch<InvalidOperationException>(() => { var i = o.Value; });
+        }
+
+		[Test]
+		public void Default_class()
+        {
+            var o = default(Option<string>);
+            o.HasValue.IsFalse();
+            Assert.Catch<InvalidOperationException>(() => { var s = o.Value; });
+        }
+
+		[Test]
+		public void Default_nullable()
+        {
+            var o = default(Option<int?>);
+            o.HasValue.IsFalse();
+            Assert.Catch<InvalidOperationException>(() => { int? ni = o.Value; });
+        }
+
+        [Test]
+        public void ToString_Some()
+        {
+            var s = "foo";
+            var o = Option.Some(s);
+
+            o.ToString().Is(string.Format("Some({0})", s.ToString()));
+        }
+
+		[Test]
+		public void ToString_None()
+        {
+            var o = Option.None<string>();
+
+            o.ToString().Is("None");
+        }
+
+        private object[] Equals_TestCaseSource =
+        {
+            new object[]{ Option.Some("foo"), Option.Some("foo"), true },
+            new object[]{ Option.Some("foo"), Option.Some("bar"), false },
+            new object[]{ Option.Some("foo"), Option.None<string>(), false },
+            new object[]{ Option.None<string>(), Option.None<string>(), true },
+        };
+        [TestCaseSource("Equals_TestCaseSource")]
+        public void Equals(Option<string> left, Option<string> right, bool equals)
+        {
+            left.Equals(right).Is(equals);
+            (left == right).Is(equals);
+            (left != right).Is(!equals);
+        }
+
+        //Option
+        [Test]
+        public void Some_Struct()
+        {
+            var o = Option.Some(1);
+            o.IsInstanceOf<Option<int>>();
+            o.HasValue.IsTrue();
+            o.Value.Is(1);
+        }
+
+        [Test]
+        public void Some_Class()
+        {
+            var o = Option.Some("foo");
+            o.IsInstanceOf<Option<string>>();
+            o.HasValue.IsTrue();
+            o.Value.Is("foo");
+        }
+
+        [Test]
+        public void Some_Nullable()
+        {
+            var o = Option.Some((int?)42);
+            o.IsInstanceOf<Option<int?>>();
+            o.HasValue.IsTrue();
+            o.Value.Is((int?)42);
+        }
+
+        [Test]
+        public void None_Struct()
+        {
+            var o = Option.None<int>();
+            o.IsInstanceOf<Option<int>>();
+            o.HasValue.IsFalse();
+            Assert.Catch<InvalidOperationException>(() => { int i = o.Value; });
+        }
+
+        [Test]
+        public void None_Class()
+        {
+            var o = Option.None<string>();
+            o.IsInstanceOf<Option<string>>();
+            o.HasValue.IsFalse();
+            Assert.Catch<InvalidOperationException>(() => { string s = o.Value; });
+        }
+
+        [Test]
+        public void None_Nullable()
+        {
+            var o = Option.None<int?>();
+            o.IsInstanceOf<Option<int?>>();
+            o.HasValue.IsFalse();
+            Assert.Catch<InvalidOperationException>(() => { int? ni = o.Value; });
+        }
+
+        private object[] Bind_TestCaseSource =
+        {
+			new object[]{ Option.Some("foo"), Option.Some(3), true },
+			new object[]{ Option.Some(""), Option.None<int>(), true },
+			new object[]{ Option.None<string>(), Option.None<int>(), false },
+		};
+        [TestCaseSource("Bind_TestCaseSource")]
+        public void Bind(Option<string> input, Option<int> expected, bool isRunning)
+        {
+            bool funcRunning = false;
+            Func<string, Option<int>> func = s =>
+            {
+                funcRunning = true;
+                if (string.IsNullOrWhiteSpace(s)) return Option.None<int>();
+                else return Option.Some(s.Length);
+            };
+
+            input.Bind(func).Is(expected);
+            funcRunning.Is(isRunning);
+        }
+
+        private object[] BindChain_TestCaseSource = 
+        {
+			new object[]{ Option.Some("fooo"), Option.Some(2), true, true },
+			new object[]{ Option.Some("foo"), Option.None<int>(), true, true },
+			new object[]{ Option.Some(""), Option.None<int>(), true, false },
+			new object[]{ Option.None<string>(), Option.None<int>(), false, false },
+		};
+        [TestCaseSource("BindChain_TestCaseSource")]
+        public void BindChain(Option<string> value, Option<int> expected, bool isFirstCalled, bool isSecondCalled)
+        {
+            bool callFirstMethod = false;
+            bool callSecondMethod = false;
+
+            Func<string, Option<int>> firstMethod = s =>
+            {
+                callFirstMethod = true;
+                if (string.IsNullOrEmpty(s)) return Option.None<int>();
+                else return Option.Some(s.Length);
+            };
+
+            Func<int, Option<int>> secondMethod = i =>
+            {
+                callSecondMethod = true;
+                if (i % 2 != 0) return Option.None<int>();
+                else return Option.Some(i / 2);
+            };
+
+            value.Bind(firstMethod)
+                .Bind(secondMethod)
+                .Is(expected);
+
+            callFirstMethod.Is(isFirstCalled);
+            callSecondMethod.Is(isSecondCalled);
+        }
+
+        private object[] BindNest_TestCaseSource = 
+        {
+			new object[]{ Option.Some(1), Option.Some(2), Option.Some(3), true, true },
+			new object[]{ Option.Some(1), Option.None<int>(), Option.None<int>(), true, false },
+			new object[]{ Option.None<int>(),  Option.Some(2), Option.None<int>(), false, false },
+			new object[]{ Option.None<int>(), Option.None<int>(), Option.None<int>(), false, false },
+		};
+        [TestCaseSource("BindNest_TestCaseSource")]
+        public void BindNest(Option<int> value1, Option<int> value2, Option<int> expected, bool isOuterCalled, bool isInnerCalled)
+        {
+            bool callOuterMethod = false;
+            bool callInnerMethod = false;
+
+            value1.Bind(outer =>
+                {
+                    callOuterMethod = true;
+
+                    return value2.Bind(inner =>
+                        {
+                            callInnerMethod = true;
+
+                            return Option.Some(outer + inner);
+                        });
+                })
+                .Is(expected);
+
+            callOuterMethod.Is(isOuterCalled);
+            callInnerMethod.Is(isInnerCalled);
+        }
+
+        private object[] BindQuery_TestCaseSource =
+        {
+            new object[] { Option.Some("foo"), Option.Some(3), Option.Some(6) },
+            new object[] { Option.Some("foo"), Option.None<int>(), Option.None<int>() },
+            new object[] { Option.None<string>(), Option.Some(3), Option.None<int>() },
+            new object[] { Option.None<string>(), Option.None<int>(), Option.None<int>() },
+        };
+        [TestCaseSource("BindQuery_TestCaseSource")]
+        public void BindQuery(Option<string> value1, Option<int> value2, Option<int> expected)
+        {
+            var result = from s in value1
+                         from i in value2
+                         select (s.Length) + i;
+
+            result.Is(expected);
+        }
+
+        [Test]
+        public void MonadLow1()
+        {
+            Func<int, Option<int>> func = i =>
+            {
+                if (i % 2 == 0) return Option.Some(i / 2);
+                else return Option.None<int>();
+            };
+
+            Option.Some(1).Bind(func).Is(func(1));
+            Option.Some(2).Bind(func).Is(func(2));
+        }
+
+        [Test]
+        public void MonadLow2()
+        {
+            Func<int, Option<int>> _return = i => Option.Some(i);
+            Option.Some(1).Bind(_return).Is(Option.Some(1));
+        }
+
+        [Test]
+        public void MonadLow3()
+        {
+            Func<int, Option<int>> func1 = i =>
+            {
+                if (i % 2 == 0) return Option.Some(i / 2);
+                else return Option.None<int>();
+            };
+
+            Func<int, Option<int>> func2 = i =>
+            {
+                if (i % 3 == 0) return Option.Some(i / 3);
+                else return Option.None<int>();
+            };
+
+            (Option.Some(6).Bind(func1)).Bind(func2).Is(Option.Some(6).Bind(i => func1(i).Bind(func2)));
+            (Option.Some(4).Bind(func1)).Bind(func2).Is(Option.Some(4).Bind(i => func1(i).Bind(func2)));
+            (Option.Some(3).Bind(func1)).Bind(func2).Is(Option.Some(3).Bind(i => func1(i).Bind(func2)));
+        }
+    }
+}
---
+++ Source/Maybe/Maybe/Option.cs
@@ -0,0 +1,104 @@
+﻿namespace System
+{
+    public struct Option<T>
+    {
+        internal Option(T value, bool hasValue)
+        {
+            _Value = value;
+            _HasValue = hasValue;
+        }
+
+        private readonly T _Value;
+
+        public T Value
+        {
+            get
+            {
+                if (!HasValue) throw new InvalidOperationException("Option object must have a value.");
+
+                return _Value;
+            }
+        }
+
+        private readonly bool _HasValue;
+
+        public bool HasValue
+        {
+            get { return _HasValue; }
+        }
+
+        public override string ToString()
+        {
+            if (HasValue) return string.Format("Some({0})", Value);
+            else return "None";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            if (HasValue)
+            {
+                return Value.GetHashCode();
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static bool operator ==(Option<T> lhs, Option<T> rhs)
+        {
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(Option<T> lhs, Option<T> rhs)
+        {
+            return !(lhs == rhs);
+        }
+    }
+
+	public static class Option
+    {
+        public static Option<T> Some<T>(T value)
+        {
+            if (value == null) throw new ArgumentNullException("value");
+
+            return new Option<T>(value, true);
+        }
+
+        public static Option<T> None<T>()
+        {
+            return new Option<T>(default(T), false);
+        }
+
+        public static Option<TResult> Bind<TSounce, TResult>(this Option<TSounce> self,
+            Func<TSounce, Option<TResult>> func)
+        {
+            if (func == null) throw new ArgumentNullException("func");
+
+            if (self.HasValue)
+            {
+                return func(self.Value);
+            }
+
+            return None<TResult>();
+        }
+
+        public static Option<TResult> Select<TSounce, TResult>(this Option<TSounce> self,
+            Func<TSounce, Option<TResult>> func)
+        {
+            return Bind(self, func);
+        }
+
+        public static Option<TResult> SelectMany<TSource, TOther, TResult>(this Option<TSource> source,
+            Func<TSource, Option<TOther>> otherSelector,
+            Func<TSource, TOther, TResult> resultSelector)
+        {
+            return source.Bind(s => otherSelector(s).Bind(o => Some(resultSelector(s, o))));
+        }
+    }
+}
