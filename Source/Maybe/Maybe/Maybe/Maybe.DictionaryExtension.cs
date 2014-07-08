adf7abd3e20bba5ce14ded87ea3b0abb338a24b2
100poisha <100poisha@gmail.com>
2014-07-09 08:35:36 +0900

    add DictionaryExtension

---
+++ Source/Maybe/Maybe/Maybe/Maybe.DictionaryExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace System
+{
+    public static partial class Maybe
+    {
+        public static Option<TValue> MaybeGetValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary,
+            TKey key)
+        {
+            return Maybe.GetValue(dictionary, key);
+        }
+
+        public static Option<TValue> GetValue<TKey, TValue>(IDictionary<TKey, TValue> dictionary,
+            TKey key)
+        {
+            if (dictionary == null) throw new ArgumentNullException("dictionary");
+
+            TValue value;
+            if (dictionary.TryGetValue(key, out value))
+            {
+                return Option.Some(value);
+            }
+
+            return Option.None<TValue>();
+        }
+    }
+}
